91686abff80f70f1ecfba42fc2c2f1893b42b390
ShrikantaMazumder <shrikantamazumder@gmail.com>
2020-11-18 11:55:24 +0600

    ready to go

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createContext, useEffect, useState } from 'react';
+import { Route, Switch, useLocation } from 'react-router-dom'
+import { getCurrentUser, handleSignOut } from './components/auth/HandleLogin';
+import Login from './components/auth/Login';
+import Booking from './components/booking/Booking';
+import Header from './components/header/Header';
+import Home from './components/home/Home';
+import PrivateRoute from './components/privateRoute/PrivateRoute';
+import Search from './components/search/Search';
 
+export const UserContext = createContext();
 function App() {
+  const location = useLocation();
+  const [user, setUser] = useState(null);
+  const [bookingInfo, setBookingInfo] = useState({});
+  useEffect(() => {
+    getCurrentUser().then(res => {
+      setUser(res)
+    })
+  }, [])
+  const signOUtUser = () => {
+    handleSignOut().then(res => {
+      setUser(res)
+    })
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <UserContext.Provider value={{ user, setUser, bookingInfo, setBookingInfo, signOUtUser }}>
+      <div className={`${location.pathname === '/' || location.pathname.includes('booking') ? "home" : ""}`}>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/booking/:id" component={Booking} />
+          <PrivateRoute path="/search/:id">
+            <Search />
+          </PrivateRoute>
+          <Route path="/login" component={Login} />
+        </Switch>
+      </div>
+    </UserContext.Provider>
   );
 }
 
---
+++ src/components/firebase.config.js
@@ -0,0 +1,13 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDKjkjfi6esctnx0DSLwFqXhlUU_3keo4U",
+  authDomain: "travel-guru-94293.firebaseapp.com",
+  databaseURL: "https://travel-guru-94293.firebaseio.com",
+  projectId: "travel-guru-94293",
+  storageBucket: "travel-guru-94293.appspot.com",
+  messagingSenderId: "1050005120972",
+  appId: "1:1050005120972:web:9b8c4feb91f02867551f69",
+  measurementId: "G-2V9WTBW7M8"
+};
+
+export default firebaseConfig;
